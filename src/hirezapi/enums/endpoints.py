1f46f2f09cd59b6bb01cdde9d6aa75bd471ba30e
Aleksi <aleksi.aaron.luoma@gmail.com>
2023-02-19 23:51:56 -0500

    Added enpoints and languages.

--- src/hirezapi/enums/endpoints.py
+++ src/hirezapi/enums/endpoints.py
@@ -1,5 +1,9 @@
-import enum
+from enum import Enum
 
 
-class HirezEndpoints(enum):
-    pass
+class Endpoints(Enum):
+
+    PALADINS = "https://api.paladins.com/paladinsapi.svc"
+    REALM_ROYALE = "https://api.realmroyale.com/realmapi.svc"
+    SMITE = "https://api.smitegame.com/smiteapi.svc"
+    STATUS_PAGE = "https://stk4xr7r1y0r.statuspage.io" #http://status.hirezstudios.com
